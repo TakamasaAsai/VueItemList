e9c181c6c506a4c86e5fd272a37b63d8d5e9b382
Takamasa Asai <takamasa.asai@fullspeedtechnologies.com>
2019-11-18 17:21:31 +0800

    succeeded to display JSON on local

--- main.js
+++ main.js
@@ -16,13 +16,7 @@ var app = new Vue ({
 		sortOrder: 1,
 		// 商品リスト
 		products: [
-			{
-				id: 1,
-				name: 'Michael<br>スマホケース',
-				price:1580, image: 'images/01.jpg',
-				delv: 0,
-				isSale: true
-			},
+			{id: 1,name: 'Michael<br>スマホケース',price:1580, image: 'images/01.jpg',delv: 0,isSale: true},
 			{id: 2, name: 'Raphael<br>スマホケース', price:1580, image: 'images/02.jpg', delv: 0, isSale: true },
 			{id: 3, name: 'Gabriel<br>スマホケース', price:1580, image: 'images/03.jpg', delv: 240, isSale: true },
 			{id: 4, name: 'Uriel<br>スマホケース', price:980, image: 'images/04.jpg', delv: 0, isSale: true },
